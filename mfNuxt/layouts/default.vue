9e11d6f4c7a622a3915820de10513695a70d3b91
stamfar <54573030+stamfar@users.noreply.github.com>
2020-05-06 13:58:11 +0200

    Fikset navbar med å lime inn direktekode fra buefy

--- mfNuxt/layouts/default.vue
+++ mfNuxt/layouts/default.vue
@@ -1,55 +1,43 @@
 <template>
-  <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
-          <img
-            src="~assets/buefy.png"
-            alt="Buefy"
-            height="28"
-          >
-        </a>
+    <b-navbar>
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                   src="~assets/logo.svg"
+                    alt="Lightweight UI components for Vue.js based on Bulma"
+                >
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="#">
+                Home
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Documentation
+            </b-navbar-item>
+            <b-navbar-dropdown label="Info">
+                <b-navbar-item href="#">
+                    About
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Contact
+                </b-navbar-item>
+            </b-navbar-dropdown>
+        </template>
 
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
+        <template slot="end">
+            <b-navbar-item tag="div">
+                 <div class="buttons">
+          <b-button class="button is-light" icon-left="fas fa-bug">
+            Rapporter en feil
+          </b-button>
+          <b-button class="button is-primary" icon-left="fas fa-info-circle">
+            Om tjenesten
+          </b-button>
         </div>
-      </div>
-    </nav>
-
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <nuxt />
-      </div>
-    </section>
-  </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
 </template>
 
 <script>
@@ -72,3 +60,11 @@ export default {
   }
 }
 </script>
+
+
+<style scoped>
+.navbar {
+  border-bottom: rgba(29, 35, 48, .15) solid 1px;
+}
+
+</style>
