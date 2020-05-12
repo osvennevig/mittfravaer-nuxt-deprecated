e0487ff81b1f8f15b1b747c3678afa11df9e3fe3
stamfar <54573030+stamfar@users.noreply.github.com>
2020-05-12 22:18:47 +0200

    Lagt til noe SEO og nettleser-tittel.

--- mfNuxt/pages/index.vue
+++ mfNuxt/pages/index.vue
@@ -14,7 +14,23 @@ import utregning from '~/components/utregning'
 import appInfo from '~/components/appInfo'
 
 export default {
+  head: {
+    title: 'MittFravær - Lynrask utregning av udokumentert fravær',
+    meta: [
+      { hid: 'description', name: 'description', content: 'Beregn fraværet ditt på sekunder' }
+    ],
+    noscript: [
+      { innerHTML: 'MittFravær gjør det enklere å finne ut hvor mange prosent fravær man har i det enkelte faget på VGS. I tillegg til å beregne hvor mye fravær man kan ha før man når 10-prosenten.', body: true }
+    ],
+    script: [
+      { src: '/head.js' },
+      // Supported since 1.0
+      { src: '/body.js', body: true },
+      { src: '/defer.js', defer: '' }
+    ]
+  },
   name: 'Forsiden',
+  
 
   components: {
     heroTopp,
