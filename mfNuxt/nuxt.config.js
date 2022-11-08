8beef331ba284fa8aac74eab64e6cc948b3efdc7
osvennevig <69307900+osvennevig@users.noreply.github.com>
2022-11-08 09:28:37 +0100

    switch google fonts with bunny cdn

--- mfNuxt/nuxt.config.js
+++ mfNuxt/nuxt.config.js
@@ -1,69 +1,70 @@
-import webpack from 'webpack'
+import webpack from "webpack";
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     htmlAttrs: {
-      lang: 'no'
+      lang: "no"
     },
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Work+Sans:400,600,700,800&display=swap' }
-      
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.bunny.net/css?family=Work+Sans:400,600,700,800&display=swap"
+      }
     ]
   },
   generate: {
     fallback: true
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
- css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
-   /* { src: '~plugins/ga.js', mode: 'client' }*/
+    /* { src: '~plugins/ga.js', mode: 'client' }*/
   ],
   /*
-  ** Nuxt.js dev-modules
-  */
- buildModules: [
-],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
 
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://buefy.github.io/#/documentation
-    'nuxt-buefy',
+    "nuxt-buefy"
   ],
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-   plugins: [
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
-  ],
-    extend (config, ctx) {
-    
-    }
+     ** You can extend webpack config here
+     */
+    plugins: [new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)],
+    extend(config, ctx) {}
   }
-}
+};
